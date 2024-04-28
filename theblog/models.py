c951a364e084ecc8d4408aa75b200f132e56a11f
Graeme Adamson <graya0277@gmail.com>
2024-04-28 23:17:22 +0000

    created base page

--- theblog/models.py
+++ theblog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=255) 
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="")  
     author = models.ForeignKey(User, on_delete=models.CASCADE) 
     body = models.TextField()
 
