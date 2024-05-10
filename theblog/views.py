b325b9780a9e8030f51ed814a3bf45cde002cb1d
Graeme Adamson <graya0277@gmail.com>
2024-05-10 21:59:40 +0000

    delete posts and order by date

--- theblog/views.py
+++ theblog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ["-post_date"]
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -23,3 +25,8 @@ class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
